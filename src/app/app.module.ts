0748da79e1e550c2070ff0c4f3d293a1fdeac3ae
puegue1999 <ra-fasilva2010@hotmail.com>
2024-03-12 10:31:57 -0300

    Third Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BannerComponent } from './component/layout1/banner/banner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BannerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
